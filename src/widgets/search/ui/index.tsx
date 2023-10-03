7a80ec093cdee428b187c73bfdeb978762a4523d
heiwa.local <heiwa.developer@gmail.com>
2023-10-03 20:58:33 +0300

    #add searchWidget template

---
+++ src/widgets/search/ui/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "./styles.module.scss"
+import cn from "classnames"
+
+type Props = {
+    className?: string
+}
+
+export const SearchWidget: React.FC<Props> = (props) => {
+    return (
+        <div className={styles.searchWidget}>
+            
+        </div>
+    )
+}
